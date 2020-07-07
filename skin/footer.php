95a016238fe4e574ce312b98e4eca42d94b24b22
Nicole Sawler <nicolesawler@gmail.com>
2020-07-07 17:51:07 -0300

    Delete footer.php

--- skin/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="clear"></div>
-        <footer>
-          <p>&copy; <?= date('Y') ?> </p>
-        </footer>
-        </div><!-- container -->
-    </div><!-- wrapper -->
-</body>
-</html>
